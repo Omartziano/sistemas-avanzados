856d597a9e97b9fc305e26026a7880d4d37b20ff
Omar Acosta <omartziano@gmail.com>
2015-09-26 17:01:06 -0500

    Homepage updates

---
+++ index.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+        <title>Doran Took My Money</title>
+        <link rel="shortcut icon" href="img/logo/poro.png">
+
+        <!-- CSS  -->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+        <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    </head>
+    <body>
+        <nav class="black" role="navigation">
+            <div class="nav-wrapper container">
+                <a id="logo-container" href="#" class="brand-logo"><img src="img/logo/porosunnyrays.png" class="display-logo"/><span>Lalelulelo</span></a>
+                <ul class="right hide-on-med-and-down tabs">
+                    <li class="tab col s3"><a href="#" class="waves-effect waves-light">Home</a></li>
+                    <li class="tab col s3"><a href="#" class="waves-effect waves-light">News</a></li>
+                    <li class="tab col s3"><a href="#" class="waves-effect waves-light">Events</a></li>
+                    <li class="tab col s3"><a href="#" class="waves-effect waves-light">Store</a></li>
+                    <li class="tab col s3"><a href="#" class="waves-effect waves-light">FAQs</a></li>
+                    <li class="tab col s3"><a href="#" class="waves-effect waves-light">Contact</a></li>
+                    <li class="tab col s3"><a href="#" class="waves-effect waves-light">LogIn</a></li>
+                </ul>
+
+                <ul id="nav-mobile" class="side-nav">
+                    <li><a href="#" class="waves-effect waves-light">Home</a></li>
+                    <li><a href="#" class="waves-effect waves-light">News</a></li>
+                    <li><a href="#" class="waves-effect waves-light">Events</a></li>
+                    <li><a href="#" class="waves-effect waves-light">Store</a></li>
+                    <li><a href="#" class="waves-effect waves-light">FAQs</a></li>
+                    <li><a href="#" class="waves-effect waves-light">Contact</a></li>
+                    <li><a href="#" class="waves-effect waves-light">LogIn</a></li>
+                </ul>
+                <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+            </div>
+        </nav>
+
+        <div class="slider">
+            <ul class="slides">
+                <li>
+                    <img src="img/slider/project_rotator_live_crop.jpg" style="top: 150px;">
+                    <div class="caption left-align">
+                        <h3>League Of Legends</h3>
+                        <h4>MEMORY REPAIR <br>// COMPLETE</h4>
+                        <h5 class="light grey-text text-lighten-3">My name is Yi</h5>
+                    </div>
+                </li>
+                <li>
+                    <img src="img/slider/bg_thebetaisover.jpg"> <!-- random image -->
+                    <div class="caption center-align">
+                        <h3>DOTA 2</h3>
+                        <h4>THE BETA IS OVER</h4>
+                        <h5 class="light grey-text text-lighten-3">Get Dota 2 on Steam</h5>
+                    </div>
+                </li>
+                <li>
+                    <img src="img/slider/reaper-of-souls.jpg"> <!-- random image -->
+                    <div class="caption left-align">
+                        <h3>DIABLO III</h3>
+                        <h4 class="light grey-text text-lighten-3">Reaper Of Souls Expansion</h4>
+                    </div>
+                </li>
+                <li>
+                    <img src="img/slider/Featureshowcase_XingTian.jpg"> <!-- random image -->
+                    <div class="caption center-align">
+                        <h3>SMITE</h3>
+                        <h4>Enter Xing Tian</h4>
+                        <h5 class="light grey-text text-lighten-3">The Relentless</h5>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        
+        <div class="container">
+            <div class="section">
+                <!--   Icon Section   -->
+                <div class="row">
+                    <div class="col s12 m4">
+                        <div class="card">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <img class="activator" src="img/news/2015.09.25.articlebanner.champskinsale.jpg">
+                            </div>
+                            <div class="card-content">
+                                <span class="card-title activator grey-text text-darken-4">Champion and skin sale: 09.25 - 09.28<i class="material-icons right">more_vert</i></span>
+                                <p><a href="#">Go</a></p>
+                            </div>
+                            <div class="card-reveal">
+                                <span class="card-title grey-text text-darken-4">Champion and skin sale: 09.25 - 09.28<i class="material-icons right">close</i></span>
+                                <p>Grab these champions and skins on sale for 50% off for a limited time!</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col s12 m4">
+                        <div class="card">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <img class="activator" src="img/news/article.header.sept_.2015.bundles_en.jpg">
+                            </div>
+                            <div class="card-content">
+                                <span class="card-title activator grey-text text-darken-4">It’s time for September bundles and Mystery skins!<i class="material-icons right">more_vert</i></span>
+                                <p><a href="#">Go</a></p>
+                            </div>
+                            <div class="card-reveal">
+                                <span class="card-title grey-text text-darken-4">It’s time for September bundles and Mystery skins!<i class="material-icons right">close</i></span>
+                                <p>September bundles and Mystery skins show up and show off!</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col s12 m4">
+                        <div class="card">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <img class="activator" src="img/news/blog_frankfurt_major_announcement.jpg">
+                            </div>
+                            <div class="card-content">
+                                <span class="card-title activator grey-text text-darken-4">Announcing The Frankfurt Major<i class="material-icons right">more_vert</i></span>
+                                <p><a href="#">Go</a></p>
+                            </div>
+                            <div class="card-reveal">
+                                <span class="card-title grey-text text-darken-4">Announcing The Frankfurt Major<i class="material-icons right">close</i></span>
+                                <p>Attendance to the Frankfurt Major will be open to the public and will not require a ticket for any day except for the day of the finals, which will take place on Saturday, November 21st. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div class="col s12 m4">
+                        <div class="card">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <img class="activator" src="img/news/685_blogimage_n54j3456n54.jpg">
+                            </div>
+                            <div class="card-content">
+                                <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+                                <p><a href="#">This is a link</a></p>
+                            </div>
+                            <div class="card-reveal">
+                                <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col s12 m4">
+                        <div class="card">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <img class="activator" src="img/news/rebornupdate_blogimage.jpg">
+                            </div>
+                            <div class="card-content">
+                                <span class="card-title activator grey-text text-darken-4">Gameplay Update 6.85 and More<i class="material-icons right">more_vert</i></span>
+                                <p><a href="#">Go</a></p>
+                            </div>
+                            <div class="card-reveal">
+                                <span class="card-title grey-text text-darken-4">Gameplay Update 6.85 and More<i class="material-icons right">close</i></span>
+                                <p>Gameplay Update 6.85 is here, bringing various balance changes to heroes and items ahead of the Frankfurt Major.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col s12 m4">
+                        <div class="card">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <img class="activator" src="img/news/UX9MW0E436OC1440435261329.jpg">
+                            </div>
+                            <div class="card-content">
+                                <span class="card-title activator grey-text text-darken-4">Reborn has arrived<i class="material-icons right">more_vert</i></span>
+                                <p><a href="#">Go</a></p>
+                            </div>
+                            <div class="card-reveal">
+                                <span class="card-title grey-text text-darken-4">Reborn has arrived<i class="material-icons right">close</i></span>
+                                <p>As of yesterday, all Dota 2 players are now running the Reborn update, which includes the Source 2 engine, a rebuilt UI, and Custom Games.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <!--<div id="index-banner" class="parallax-container">
+            <div class="section no-pad-bot">
+                <div class="container">
+                    <br><br>
+                    <h1 class="header center teal-text text-lighten-2">Parallax Template</h1>
+                    <div class="row center">
+                        <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                    </div>
+                    <div class="row center">
+                        <a href="#" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">Get Started</a>
+                    </div>
+                    <br><br>
+
+                </div>
+            </div>
+            <div class="parallax"><img src="img/project_rotator_live.jpg" alt="Unsplashed background img 1" style="display: block; transform: translate3d(-35%, 200px, 0px);"></div>
+        </div>-->
+
+
+        <div class="container">
+            <div class="section">
+                <!--   Icon Section   -->
+                <div class="row">
+                    <div class="col s12 m4">
+                        <div class="icon-block">
+                            <h2 class="center brown-text"><i class="material-icons">flash_on</i></h2>
+                            <h5 class="center">Speeds up development</h5>
+
+                            <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+                        </div>
+                    </div>
+
+                    <div class="col s12 m4">
+                        <div class="icon-block">
+                            <h2 class="center brown-text"><i class="material-icons">group</i></h2>
+                            <h5 class="center">User Experience Focused</h5>
+
+                            <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+                        </div>
+                    </div>
+
+                    <div class="col s12 m4">
+                        <div class="icon-block">
+                            <h2 class="center brown-text"><i class="material-icons">settings</i></h2>
+                            <h5 class="center">Easy to work with</h5>
+
+                            <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+<!--        <div class="parallax-container valign-wrapper">
+            <div class="section no-pad-bot">
+                <div class="container">
+                    <div class="row center">
+                        <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="section">
+
+                <div class="row">
+                    <div class="col s12 center">
+                        <h3><i class="mdi-content-send brown-text"></i></h3>
+                        <h4>Contact Us</h4>
+                        <p class="left-align light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>-->
+
+
+        <div class="parallax-container valign-wrapper">
+            <div class="section no-pad-bot">
+                <div class="container">
+                    <div class="row center">
+                        <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <footer class="page-footer teal">
+            <div class="container">
+                <div class="row">
+                    <div class="col l6 s12">
+                        <h5 class="white-text">Company Bio</h5>
+                        <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+
+
+                    </div>
+                    <div class="col l3 s12">
+                        <h5 class="white-text">Settings</h5>
+                        <ul>
+                            <li><a class="white-text" href="#!">Link 1</a></li>
+                            <li><a class="white-text" href="#!">Link 2</a></li>
+                            <li><a class="white-text" href="#!">Link 3</a></li>
+                            <li><a class="white-text" href="#!">Link 4</a></li>
+                        </ul>
+                    </div>
+                    <div class="col l3 s12">
+                        <h5 class="white-text">Connect</h5>
+                        <ul>
+                            <li><a class="white-text" href="#!">Link 1</a></li>
+                            <li><a class="white-text" href="#!">Link 2</a></li>
+                            <li><a class="white-text" href="#!">Link 3</a></li>
+                            <li><a class="white-text" href="#!">Link 4</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-copyright">
+                <div class="container">
+                    Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+                </div>
+            </div>
+        </footer>
+
+
+        <!--  Scripts-->
+        <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script src="js/materialize.js"></script>
+        <script src="js/init.js"></script>
+        <script>
+            $(document).ready(function () {
+                $('.slider').slider({full_width: true});
+                $('ul.tabs').tabs('select_tab', 'tab_id');
+            });
+        </script>
+
+    </body>
+</html>
+
