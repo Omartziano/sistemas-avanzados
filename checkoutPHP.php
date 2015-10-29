b4908b21d3f853e68ad00ca9d10314596ccccedd
joako0z <joako2393@hotmail.com>
2015-10-29 05:17:07 -0600

    ya inserta en la tabla venta solo falta detalle de venta que que no sepuede leer la variable de sesion $_SESSION['cartProducts'] desde este archivo

--- checkoutPHP.php
+++ checkoutPHP.php
@@ -24,47 +24,39 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 //--------------------------comienza joako
-
-//esto lo hice para sacar el id del usuario con el nombre del usuario porque no sabia si estaba en alguna variable de sesion o no X_X
-$idUsuario;
-$query = "SELECT idUsuario FROM usuario where usuarioUsuario=" . $postUserName;
-$execute = mysql_query($query, $conn);
-if ($row = mysql_fetch_array($execute)) {
-    //Guardo los datos de la BD en las variables de php
-    $idUsuario = $row["idUsuario"];
-}
-
-//se inserta primero la venta para asociarlla con los detalles de venta
-    $sql = "INSERT INTO venta values (null,'" . $postFechaVta . "', '" . $postImporteVta . "'," . $idUsuario . ")";//le quite las comillas simples al id usuario porque es numero no se si este bien 
-    
+//se inserta primero la venta para asociarlla con los detalles de ventas
+$sql = "INSERT INTO venta values (null,NOW(), CAST((SUBSTRING('" . $postImporteVta . "',2,30)) AS DECIMAL(65,2))," . "(SELECT idUsuario FROM usuario where usuarioUsuario='" . $postUserName . "'))"; //le quite las comillas simples al id usuario porque es numero no se si este bien 
 //se recorren los articulos del carrito y se va haciendo el insert por cada  uno
-    
-    $idVenta="SELECT idVenta FROM VENTA WHERE fechaVenta=". $postFechaVta ."and idUsuario=". $idUsuario.")";
+
+$idVenta = "SELECT idVenta FROM VENTA WHERE fechaVenta=" . $postFechaVta . "and idUsuario= (SELECT idUsuario FROM usuario where usuarioUsuario='" . $postUserName . "'))";
+
 for ($row = 0; $row < sizeof($_SESSION['cartProducts']); $row++) {
 
-        $realRow = $row + 1;
-        for ($col = 0; $col < sizeof($_SESSION['cartProducts'][$row]); $col++) {
-            //aqui se llenan las variables que se necesitan para el insert
-            //$idProducto
-            //$cantidad
+    $realRow = $row + 1;
+    for ($col = 0; $col < sizeof($_SESSION['cartProducts'][$row]); $col++) {
+        //echo "\n".$_SESSION['cartProducts'][$row][$col];
+        if (col == 1) {
+            $nombreProducto = $_SESSION['cartProducts'][$row][$col];
+        } else if (col == 4) {
+            $cantidad = $_SESSION['cartProducts'][$row][$col];
         }
-        
-        //se agrega el registro 
-        $sql = "INSERT INTO detalleventa values (". $idVenta . ", " . $idProducto . "," . $cantidad . ")";
     }
+
+    //se agrega el registro 
+    $sql = "INSERT INTO detalleventa values (" . $idVenta . ",SELECT idProducto FROM usuario where nombreProducto='" . $nombreProducto . "')," . $cantidad . ")";
+}
 
 
 
 //---------------------------------termina joako
 
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-        session_set_cookie_params(0);
-        session_start();
-        $_SESSION["username"] = $postUsername;
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+    session_set_cookie_params(0);
+    session_start();
+    $_SESSION["username"] = $postUsername;
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
-    $conn->close();
-    
+$conn->close();
